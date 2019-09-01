36eb2d4d69cd7cd3fce7165d748d471c5be55aeb
KERWIN <75631591@qq.com>
2019-09-01 14:09:03 +0800

    调整小程序服务端价格，启用maven

---
+++ OUTBREAK_1_server/src/main/java/server/OUTBREAK_1/App.java
@@ -0,0 +1,13 @@
+package server.OUTBREAK_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
