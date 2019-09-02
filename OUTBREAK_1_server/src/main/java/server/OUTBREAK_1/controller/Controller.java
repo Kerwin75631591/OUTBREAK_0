7c85168ee8fc69c6774942ee2d30dad8c1f23c45
KERWIN <75631591@qq.com>
2019-09-02 15:06:15 +0800

    usertable新增duties职务

--- OUTBREAK_1_server/src/main/java/server/OUTBREAK_1/controller/Controller.java
+++ OUTBREAK_1_server/src/main/java/server/OUTBREAK_1/controller/Controller.java
@@ -58,6 +58,47 @@ public class Controller {
 		return map;
 	}
 	
-	
+	//注册功能
+	@RequestMapping("Register")
+	public Map<String, Object> Register(String email,String password,String name){
+		Map<String, Object> map = new HashMap<String, Object>();
+		List<String> list = new ArrayList<String>();
+		DBConnect db=new DBConnect();
+		try {
+			db.connect();
+			System.out.println("数据库连接成功");
+		} catch (SQLException e) {
+			System.out.println("数据库连接失败");
+			e.printStackTrace();
+		}
+		try {
+			if(db.searchUser(false, email, password)==0)
+			{
+				db.insertUser(email, password, null, name, null);
+				ResultSet rs=db.searchPeople(email);
+				List<Integer> listtemp=new ArrayList<Integer>();
+				while(rs.next()) {
+					listtemp.add(rs.getInt("mid"));
+				}
+				for (int i = 0; i < listtemp.size(); i++) {
+					 rs=db.searchMeeting(listtemp.get(i));
+					 rs.next();
+					 list.add(rs.getString("name"));
+					 list.add(""+rs.getDate("begintime"));
+					 list.add(rs.getString("place"));
+					 list.add(""+rs.getInt("state"));
+					 map.put("list"+i,list);
+					 list.clear();
+					 }
+			}else {
+				String message="注册失败";
+				map.put("message", message);
+			}
+		} catch (SQLException e) {
+			System.out.println("登录搜索失败");
+			e.printStackTrace();
+		}
+		return map;
+	}
 
 }
